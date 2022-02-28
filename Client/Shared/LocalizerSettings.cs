d3276604731b21ffd6496393d78f08d4f338d48e
Aml_Abbas <aml.abbas@hotmail.com>
2022-02-28 14:16:41 +0100

    Add project files.

---
+++ Client/Shared/LocalizerSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AljabhaAldemokratiaAlshaabia.Client.Shared
+{
+    public static class LocalizerSettings
+    {
+        public const string NeutralCulture = "en-US";
+
+        public static readonly string[] SupportedCultures = { NeutralCulture, "ar-AR"};
+
+        public static readonly (string, string)[] SupportedCulturesWithName = new[] { ("English", NeutralCulture), ("Arabic", "ar-AR")};
+    }
+}
