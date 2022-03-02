5af20c82b8b98bc1dd94eb20e2665a6003f581a6
Aml_Abbas <aml.abbas@hotmail.com>
2022-03-02 11:36:54 +0100

    ...

---
+++ Client/FileUtil .cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AljabhaAldemokratiaAlshaabia.Client
+{
+    public static class FileUtil
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, string url)
+       => js.InvokeAsync<object>(
+           "triggerFileDownload",
+           filename,
+           url);
+    }
+}
