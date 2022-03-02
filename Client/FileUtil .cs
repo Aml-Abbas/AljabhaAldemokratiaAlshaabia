78897bd2ebcfac68075e37d01fe76f1c82afbb6b
Aml_Abbas <aml.abbas@hotmail.com>
2022-03-02 12:06:58 +0100

    ---

--- Client/FileUtil .cs
+++ Client/FileUtil .cs
@@ -8,10 +8,10 @@ namespace AljabhaAldemokratiaAlshaabia.Client
 {
     public static class FileUtil
     {
-        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, string url)
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
        => js.InvokeAsync<object>(
            "triggerFileDownload",
            filename,
-           url);
+           Convert.ToBase64String(data));
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -19,6 +19,7 @@ namespace AljabhaAldemokratiaAlshaabia.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<WeatherForecastService>();
 
             builder.Services.AddLocalization();
             builder.Services.AddBlazoredLocalStorage();
